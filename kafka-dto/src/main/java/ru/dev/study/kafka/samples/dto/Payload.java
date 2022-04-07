027ca4bb065b9d638d00d08f27974360c8483177
Yuri Nesterov <nesteroviury@gmail.com>
2022-04-07 08:21:20 +0400

    kafka dto module changed

--- kafka-dto/src/main/java/ru/dev/study/kafka/samples/dto/Payload.java
+++ kafka-dto/src/main/java/ru/dev/study/kafka/samples/dto/Payload.java
@@ -1,11 +1,18 @@
 package ru.dev.study.kafka.samples.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@ToString
 public class Payload {
-    private LocalDateTime createDateTime;
+    private LocalDateTime createDateTime = LocalDateTime.now();
+    @Getter
     private String data;
+
+    public Payload(String data) {
+        this.data = data;
+    }
 }
